33ba27697b0f1c703311378bdbfd847572884c6d
Daniel <dplopez.sioux@gmail.com>
2021-05-31 02:16:36 -0500

    onlinux

--- src/component/experience.js
+++ src/component/experience.js
@@ -1,65 +1,67 @@
 import React from "react";
+import "./css/experience.css";
+import server from "../img/server.png";
 
 import * as Icon from "react-bootstrap-icons";
 
 const Experience = () => {
   return (
-    <div className="container">
-      <div className="shadow-lg p-5 m-5 bg-body rounded text-center">
-        <h1 className="display-6 fw-bold text-dark">
-          The server configuration for this example!
-        </h1>
-        <div className="col-lg-6 mx-auto">
-          <div className="container">
-            <div className="row">
-              <div className="col order-first">
-                <p className="mb-4">
-                  <Icon.Server />
-                  The app runs on{" "}
-                  <a href="#" className="link-light">
-                    AWS
-                  </a>
-                  EC2, where a public ip address was assigned and the domain was
-                  purchased.
-                </p>
-              </div>
-              <div className="col">
-                <p className="mb-4">
-                  The addressing configuration was implemented with
-                  <Icon.Bricks />
-                  <a href="https://unit.nginx.org/" className="link-info">
-                    {" "}
-                    NGinx{" "}
-                  </a>{" "}
-                  and SSL was generated with <Icon.Key />
-                  <a href="https://letsencrypt.org/ " className="link-info">
-                    {" "}
-                    LetsEncrypt
-                  </a>
-                  .
-                </p>
-              </div>
-              <div className="col order-last">
-                <p>
-                  I also have experience in Heroku, BlueHost, GoDaddy, Ionos.
-                </p>
-              </div>
+    <div className="position-relative container ">
+      <div className="shadow overflow-hidden p-5 mt-5 text-center bg-light">
+        <div className="col-md-10  mx-auto my-5 palante">
+          <h1 className="mt-3 display-6 fw-bold text-dark">
+            The server configuration for this example!
+          </h1>
+
+          <div class="p-3 mt-3 row align-items-center">
+            <div class="col">
+              <p>I also have experience in Heroku, BlueHost, GoDaddy, Ionos.</p>
+              <img
+                className="mx-500"
+                src="https://www.nginx.com/wp-content/uploads/2021/05/NGINX-Service-Mesh-vertical-white-type.svg"
+              ></img>
+            </div>
+            <div class="col">
+              <p>I also have experience in Heroku, BlueHost, GoDaddy, Ionos.</p>
+              <img src="https://api.iconify.design/logos-aws-ec2.svg"></img>
+            </div>
+            <div class="col">
+              <p>I also have experience in Heroku, BlueHost, GoDaddy, Ionos.</p>
+              <img src="https://www.nginx.com/wp-content/uploads/2021/05/NGINX-Service-Mesh-vertical-white-type.svg"></img>
+            </div>
+            <div class="col">
+              <p>I also have experience in Heroku, BlueHost, GoDaddy, Ionos.</p>
+              <img src="https://www.nginx.com/wp-content/uploads/2021/05/NGINX-Service-Mesh-vertical-white-type.svg"></img>
+            </div>
+            <div class="col">
+              <p>I also have experience in Heroku, BlueHost, GoDaddy, Ionos.</p>
+              <img src="https://www.nginx.com/wp-content/uploads/2021/05/NGINX-Service-Mesh-vertical-white-type.svg"></img>
+            </div>
+            <div class="col">
+              <p>I also have experience in Heroku, BlueHost, GoDaddy, Ionos.</p>
+              <img src="https://www.nginx.com/wp-content/uploads/2021/05/NGINX-Service-Mesh-vertical-white-type.svg"></img>
             </div>
           </div>
-          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <div className="btn-group">
-              <a
-                href="https://github.com/dplopezsioux/resume"
-                className="btn btn-primary"
-                aria-current="page"
-                target="blank"
-              >
-                Active link
-              </a>
-            </div>
-          </div>
+
+          <h1 className="display-4 fw-normal palante">Punny headline</h1>
+          <p className="lead fw-normal">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <a className="btn btn-outline-secondary" href="#">
+            Coming soon
+          </a>
         </div>
       </div>
+
+      <img className="product-device d-none d-md-block" src={server}></img>
     </div>
   );
 };
