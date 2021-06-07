f7bec5850babd05bfbe6dad65eb19a60fba81d63
Daniel <dplopez.sioux@gmail.com>
2021-06-07 03:12:17 -0500

    renderiza los skill ok

--- src/component/skill.js
+++ src/component/skill.js
@@ -1,98 +1,90 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const dataTest = [
-  {
-    tecnologies: [
-      {
-        name: "nodejs",
-        type: "Tecnologies",
-        url: "https://nodejs.org/en/",
-        descrip:
-          "Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
-      },
-      {
-        name: "mySQL",
-        type: "Tecnologies",
-        url: "https://www.mysql.com/",
-        descrip: " 2021, Oracle Corporation and/or its affiliates",
-      },
-      {
-        name: "mongoDb",
-        type: "Tecnologies",
-        url: "https://www.mongodb.com/",
-        descrip:
-          "mongoDB is a general purpose, document-based, distributed database built for modern application developers",
-      },
-    ],
-  },
-  {
-    languages: [
-      {
-        name: "Javascript",
-
-        type: "languages",
-        url: "https://www.javascript.com/",
-        descrip:
-          "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
-      },
-      {
-        name: "Typescript",
-
-        type: "languages",
-        url: "https://www.javascript.com/",
-        descrip:
-          "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
-      },
-      {
-        name: "C#",
-
-        type: "languages",
-        url: "https://www.javascript.com/",
-        descrip:
-          "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
-      },
-      {
-        name: "C++",
-        type: "languages",
-        url: "https://www.javascript.com/",
-        descrip:
-          "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
-      },
-    ],
-  },
-  {
-    tools: [
-      {
-        name: "Github",
-        type: "Tools",
-        url: "https://www.javascript.com/",
-        descrip:
-          "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
-      },
-      {
-        name: "mongoDb",
-        type: "Tools",
-        url: "https://www.javascript.com/",
-        descrip:
-          "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
-      },
-      {
-        name: "MySql",
-        type: "Tools",
-        url: "https://www.javascript.com/",
-        descrip:
-          "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
-      },
-      {
-        name: "SqlServer",
-        type: "Tools",
-        url: "https://www.javascript.com/",
-        descrip:
-          "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
-      },
-    ],
-  },
-];
+const dataTest = {
+  tecnologies: [
+    {
+      name: "nodejs",
+      type: "Tecnologies",
+      url: "https://nodejs.org/en/",
+      descrip:
+        "Node.js® is a JavaScript runtime built on Chrome's V8 JavaScript engine.",
+    },
+    {
+      name: "mySQL",
+      type: "Tecnologies",
+      url: "https://www.mysql.com/",
+      descrip: " 2021, Oracle Corporation and/or its affiliates",
+    },
+    {
+      name: "mongoDb",
+      type: "Tecnologies",
+      url: "https://www.mongodb.com/",
+      descrip:
+        "mongoDB is a general purpose, document-based, distributed database built for modern application developers",
+    },
+  ],
+  languages: [
+    {
+      name: "Javascript",
+      type: "languages",
+      url: "https://www.javascript.com/",
+      descrip:
+        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+    },
+    {
+      name: "Typescript",
+      type: "languages",
+      url: "https://www.javascript.com/",
+      descrip:
+        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+    },
+    {
+      name: "C#",
+      type: "languages",
+      url: "https://www.javascript.com/",
+      descrip:
+        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+    },
+    {
+      name: "C++",
+      type: "languages",
+      url: "https://www.javascript.com/",
+      descrip:
+        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+    },
+  ],
+  tools: [
+    {
+      name: "Github",
+      type: "Tools",
+      url: "https://www.javascript.com/",
+      descrip:
+        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+    },
+    {
+      name: "mongoDb",
+      type: "Tools",
+      url: "https://www.javascript.com/",
+      descrip:
+        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+    },
+    {
+      name: "MySql",
+      type: "Tools",
+      url: "https://www.javascript.com/",
+      descrip:
+        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+    },
+    {
+      name: "SqlServer",
+      type: "Tools",
+      url: "https://www.javascript.com/",
+      descrip:
+        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+    },
+  ],
+};
 
 //programacion
 // npm install --save-dev @iconify/react @iconify-icons/ion
@@ -100,42 +92,55 @@ import { Icon } from "@iconify/react";
 
 import settingsSharp from "@iconify-icons/ion/settings-sharp";
 
-const TableInterior = () => {
-  return (
-    <div>
-      <div className="my-3 p-3 bg-light rounded shadow-lg">
-        <h6 claclassNamess="border-bottom pb-2 mb-0">"dato[0].type"</h6>
-        <div className="d-flex text-muted pt-3">
-          <svg
-            className="bd-placeholder-img flex-shrink-0 me-2 rounded"
-            width="32"
-            height="32"
-            xmlns="http://www.w3.org/2000/svg"
-            role="img"
-            aria-label="Placeholder: 32x32"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill={getRandomColor()}></rect>
-            <text x="50%" y="50%" fill="#007bff" dy=".3em"></text>
-          </svg>
+class Listskill extends React.Component {
+  render() {
+    const listas = this.props.list;
+    const color = this.props.gcolor;
+    console.log(listas);
 
-          <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
-            <div className="d-flex justify-content-between">
-              <strong className="text-gray-dark">dato.name</strong>
-              <a href="dato.url">Link to ...</a>
+    return (
+      <div>
+        {listas.map((a) => {
+          return (
+            <div key={Math.random() * 10000} className="d-flex text-muted pt-3">
+              <svg
+                className="bd-placeholder-img flex-shrink-0 me-2 rounded"
+                width="32"
+                height="32"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                aria-label="Placeholder: 32x32"
+                preserveAspectRatio="xMidYMid slice"
+                focusable="false"
+              >
+                <title>a.name</title>
+                <rect width="100%" height="100%" fill={color}></rect>
+                <text x="50%" y="50%" fill="#007bff" dy=".3em"></text>
+              </svg>
+
+              <div className="pb-3 mb-0 small lh-sm  w-100">
+                <div className="d-flex justify-content-between">
+                  <strong className="text-gray-dark">{a.name}</strong>
+                  <a target="_blank" href={a.url}>
+                    got to ...
+                  </a>
+                </div>
+                <span className="d-block">{a.descrip}</span>
+              </div>
             </div>
-            <span className="d-block">@dato.name</span>
-          </div>
-        </div>
+          );
+        })}
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
-const Tablee = (dato) => {
-  function getRandomColor() {
+class TablaSkill extends React.Component {
+  static propTypes = {
+    dataskill: PropTypes.object.isRequired,
+  };
+
+  getRandomColor() {
     var letters = "0123456789ABCDEF";
     var color = "#";
     for (var i = 0; i < 6; i++) {
@@ -143,47 +148,27 @@ const Tablee = (dato) => {
     }
     return color;
   }
-  return (
-    <div>
-      {console.log(dato)}
-      <div className="my-3 p-3 bg-light rounded shadow-lg">
-        <h6 claclassNamess="border-bottom pb-2 mb-0">Type of Tec</h6>
-        <div className="d-flex text-muted pt-3">
-          <svg
-            className="bd-placeholder-img flex-shrink-0 me-2 rounded"
-            width="32"
-            height="32"
-            xmlns="http://www.w3.org/2000/svg"
-            role="img"
-            aria-label="Placeholder: 32x32"
-            preserveAspectRatio="xMidYMid slice"
-            focusable="false"
-          >
-            <title>Placeholder</title>
-            <rect width="100%" height="100%" fill="#00ff00"></rect>
-            <text x="50%" y="50%" fill="#007bff" dy=".3em"></text>
-          </svg>
 
-          <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
-            <div className="d-flex justify-content-between">
-              <strong className="text-gray-dark">dato.name</strong>
-              <a href="dato.url">Link to ...</a>
-            </div>
-            <span className="d-block">@dato.name</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
-
-class Prueba extends React.Component {
   render() {
-    const propsS = this.props;
-    const names = Object.keys(propsS.inform);
-    names.map();
-    console.log(Object.keys(propsS.inform));
-    return <div>WHATS</div>;
+    const names = Object.keys(this.props.dataskill);
+    const props = this.props.dataskill;
+
+    console.log(props);
+    return (
+      <div className="my-3 p-3 bg-light rounded shadow-lg">
+        {names.map((keyname) => {
+          return (
+            <div key={Math.random() * 10000}>
+              <h6 className="border-bottom pb-2 mb-0 text-uppercase">
+                {keyname}
+              </h6>
+
+              <Listskill list={props[keyname]} gcolor={this.getRandomColor()} />
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
@@ -202,37 +187,9 @@ class Skill extends React.Component {
                 <span className="fs-4">Skills and Tools</span>
               </a>
             </header>
-            <div className="my-3 p-3 bg-light rounded shadow-lg">
-              <h6 className="border-bottom pb-2 mb-0">Suggestions</h6>
-              <div className="d-flex text-muted pt-3">
-                <svg
-                  className="bd-placeholder-img flex-shrink-0 me-2 rounded"
-                  width="32"
-                  height="32"
-                  xmlns="http://www.w3.org/2000/svg"
-                  role="img"
-                  aria-label="Placeholder: 32x32"
-                  preserveAspectRatio="xMidYMid slice"
-                  focusable="false"
-                >
-                  <title>Placeholder</title>
-                  <rect width="100%" height="100%" fill="00ff00"></rect>
-                  <text x="50%" y="50%" fill="#007bff" dy=".3em"></text>
-                </svg>
 
-                <div className="pb-3 mb-0 small lh-sm border-bottom w-100">
-                  <div className="d-flex justify-content-between">
-                    <strong className="text-gray-dark">Full Name</strong>
-                    <a href="#">Follow</a>
-                  </div>
-                  <span className="d-block">@username</span>
-                </div>
-              </div>
-            </div>
             <div>
-              {dataTest.map((data) => (
-                <Prueba inform={data} key={Math.random() * 10000} />
-              ))}
+              <TablaSkill dataskill={dataTest} key={Math.random() * 10000} />
             </div>
           </div>
         </div>
