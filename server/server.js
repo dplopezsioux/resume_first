2d3b39268cb22b42bfe0707133f0eee10a9e3d6d
Daniel <dplopez.sioux@gmail.com>
2021-06-08 13:51:58 -0500

    envios de email

--- server/server.js
+++ server/server.js
@@ -1,33 +1,88 @@
+const { json } = require("body-parser");
 const express = require("express");
-const bodyParser = require("body-parser");
 const dataTest = require("./loadskill");
 const app = express();
+//
+//
 
-app.use(bodyParser.json());
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//
+//
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
+//
+//
+//
+//
+//
 
 app.get("/", function (req, res) {
   res.render("pages/index");
 });
+//
+//
+//
+//
+//
+
+const sendEmail = (email, mens) => {
+  return "OK";
+};
+
+app.get("/contact", (req, res) => {
+  console.log(req.query.email);
+  console.log(req.query.mens);
+
+  let email = req.query.email;
+  let mens = req.query.mens;
+
+  let response = sendEmail(email, mens);
+
+  res.send(response);
+
+  res.end();
+});
+
+//
+//
+//
+//
+//
 
 app.get("/api", (req, res) => {
   res.status(200).json({ mens: "Welcome to my rest API" });
 });
 
+//
+//
+//
+//
 //load data
 require("./loadskill");
 const data = dataTest;
+//
+//
+//
+//
 //sent to api/skill
 app.get("/api/skill", (req, res) => {
-  res.status(200).json(data);
+  return res.send(data);
+});
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//localhost 3000
+app.get("/api/contact", (req, res) => {
+  let values = req.body;
+  console.log(values);
 });
 
-//localhost 3000
 app.listen(3000);
