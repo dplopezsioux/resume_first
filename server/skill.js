85caa837c5e10de923b1cbd67b3a0725dd55796b
Daniel <dplopez.sioux@gmail.com>
2021-06-10 00:26:34 -0500

    skill fix it!

--- server/skill.js
+++ server/skill.js
@@ -32,53 +32,53 @@ const skillData = {
     {
       name: "Typescript",
       type: "languages",
-      url: "https://www.javascript.com/",
+      url: "https://www.typescriptlang.org/",
       descrip:
-        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+        "TypeScript is an open-source language which builds on JavaScript, one of the world’s most used tools, by adding static type definitions.",
     },
     {
       name: "C#",
       type: "languages",
-      url: "https://www.javascript.com/",
+      url: "https://docs.microsoft.com/en-us/dotnet/csharp/",
       descrip:
-        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+        "C# is a general-purpose, multi-paradigm programming language encompassing static typing, strong typing, lexically scoped, imperative, declarative, functional, generic, object-oriented, and component-oriented programming disciplines.",
     },
     {
       name: "C++",
       type: "languages",
-      url: "https://www.javascript.com/",
+      url: "https://isocpp.org/",
       descrip:
-        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+        "Standard C++ Foundation (Standard CPP Foundation in some databases that don’t support + in names) is a Washington 501(c)(6) not-for-profit organization whose purpose is to support the C++ software developer community and promote the understanding and use of modern Standard C++ on all compilers and platforms.",
     },
   ],
   tools: [
     {
       name: "Github",
       type: "Tools",
-      url: "https://www.javascript.com/",
+      url: "https://github.com/",
       descrip:
-        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+        "GitHub is where over 65 million developers shape the future of software, together​. Contribute to the open source community, manage your Git repositories, ...",
     },
     {
       name: "mongoDb",
       type: "Tools",
-      url: "https://www.javascript.com/",
+      url: "https://www.mongodb.com/",
       descrip:
-        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+        "MongoDB is a document database, which means it stores data in JSON-like documents. We believe this is the most natural way to think about data, and is much ...",
     },
     {
       name: "MySql",
       type: "Tools",
-      url: "https://www.javascript.com/",
+      url: "https://www.mysql.com/",
       descrip:
-        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+        "MySQL Database Service is a fully managed database service to deploy cloud-​native applications. HeatWave, an integrated, high-performance query ...",
     },
     {
       name: "SqlServer",
       type: "Tools",
-      url: "https://www.javascript.com/",
+      url: "https://www.microsoft.com/en-us/sql-server/sql-server-downloads",
       descrip:
-        "JavaScript.com is a resource built by the Pluralsight team for the JavaScript community.",
+        "Developer. SQL Server 2019 Developer is a full-featured free edition, licensed for use as a development and test database in a non-production environment ...",
     },
   ],
 };
